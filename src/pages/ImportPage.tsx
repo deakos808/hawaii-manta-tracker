5d9f672a31d1d6c8c9511bb63819a07e2200cd5c
deakos808 <markdeakos@gmail.com>
2025-05-11 17:55:19 -1000

    Add Import page and routing setup

---
+++ src/pages/ImportPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ImportPage() {
+  return (
+    <div style={{ padding: '2rem', color: 'white' }}>
+      <h1>Import Page</h1>
+      <p>This is where you'll upload manta ray photos.</p>
+    </div>
+  );
+}
